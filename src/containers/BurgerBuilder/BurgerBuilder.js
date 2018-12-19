a3f9e63c648cd43e80d640e0c1c1b5358f6fecc6
AlQuraian <m4design@gmail.com>
2018-12-19 18:04:45 +0100

    More summary

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -79,7 +79,8 @@ class BurgerBuilder extends React.Component {
           modalClosed={this.purchaseCancelHandler}>
           <OrderSummary ingredients={this.state.ingredients}
             purchaseCanceled={this.purchaseCancelHandler}
-            purchaseContinued={this.purchaseContinueHandler} />
+            purchaseContinued={this.purchaseContinueHandler}
+            price={this.state.totalPrice} />
         </Modal>
         <Burger ingredients={this.state.ingredients} />
         <BuildControls
